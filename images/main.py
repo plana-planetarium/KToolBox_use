9271ce55303211cecf4b7cbd05d1f850db858aa0
plana-planetarium <3412294524@qq.com>
2024-02-03 14:53:54 +0800

    update

---
+++ images/main.py
@@ -0,0 +1,23 @@
+import os
+
+url = os.getenv("URL").replace(' ', '').split(',')
+user_server = []
+user_name = []
+
+for line in url:
+    if url != '':
+        os.system("ktoolbox sync-creator " + line)
+        user_server += [url.split('/user/')[0].replace('https://kemono.su/', '')]
+        dir_file = os.listdir('./images').remove('main.py')
+        for i in dir_file:
+            if i not in user_name:
+                user_name += [i]
+
+i = 0
+user = 'kemono_' + user_server[i] + '-' + user_name[i]
+i += 1
+while i <= len(user_server) + 1:
+    user += '_' + user_server[i] + '-' + user_name[i]
+    i += 1
+    
+os.environ["USER"] = user
